26448008507ce4ce4977d942e9438b211b0ae70e
taiwo2 <38242590+taiwo2@users.noreply.github.com>
2020-07-02 12:43:02 +0100

    Add files via upload

---
+++ strange-advertising.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    ppl=[2]
+    for _ in range(n-1):
+        ppl.append(ppl[-1]* 3//2)
+        
+    return(sum(ppl))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
