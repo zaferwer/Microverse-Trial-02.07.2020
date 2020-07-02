19931b970a183623ea91b6ca0ee7a28c9efd95eb
zaferwer <67260479+zaferwer@users.noreply.github.com>
2020-07-02 14:47:07 +0300

    Update sunnydays.py

--- sunnydays.py
+++ sunnydays.py
@@ -9,6 +9,7 @@ import sys
 # Complete the beautifulDays function below.
 def beautifulDays(i, j, k):
     final = 0
+    
     for i in range(i, j + 1):
         if (i-(int(str(i)[::-1]))) % k == 0:
             final +=1
